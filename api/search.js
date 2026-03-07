11301e1739812adec399a38aa7240cf5fd3a57d7
David <windowsgames1000@gmail.com>
2026-03-07 22:27:26 +0700

    Update search.js

--- api/search.js
+++ api/search.js
@@ -119,12 +119,7 @@ export default async function handler(req, res) {
 } 
     /* ---------- FILTER AMAZON PRODUCT LINKS ---------- */
 
-    const filtered = allItems.filter(item =>
-      item.link &&
-      item.link.includes("amazon.com") &&
-      (item.link.includes("/dp/") || item.link.includes("/gp/product/"))
-    );
-
+    
     /* ---------- MAP PRODUCTS ---------- */
 
     const products = filtered.map((item, i) => {
