38b4b6dba4910380e9bb7263b996e35d51375599
David <windowsgames1000@gmail.com>
2026-03-10 00:07:09 +0700

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -4,32 +4,32 @@ export default async function handler(req, res) {
 
   try {
 
-    const dataRes = await fetch(`${base}/api/search`);
-    const data = await dataRes.json();
+    const response = await fetch(`${base}/api/search`);
+    const data = await response.json();
 
     const products = data.products || [];
 
     const urls = products.map(p => {
       return `
-<url>
-<loc>${base}/s.html?id=${p.id}</loc>
-<changefreq>daily</changefreq>
-<priority>0.8</priority>
-</url>`;
+      <url>
+        <loc>${base}/s.html?id=${p.id}</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+      </url>`;
     }).join("");
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls}
-</urlset>`;
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls}
+    </urlset>`;
 
     res.setHeader("Content-Type", "text/xml");
     res.status(200).send(xml);
 
-  } catch (err) {
+  } catch (error) {
 
-    res.status(500).send("Error generating sitemap");
+    res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>`);
 
   }
-
 }
