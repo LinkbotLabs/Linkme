add72ba3060c882d13e8014bad6bc33ebbef0899
David <windowsgames1000@gmail.com>
2026-03-11 09:00:35 +0700

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -9,33 +9,42 @@ const data = await response.json();
 
 const products = data.products || [];
 
-const urls = products.map(p => `
-<url>
+const urls = products.map(p => {
+return `<url>
 <loc>${base}/s.html?id=${p.id}</loc>
 <changefreq>daily</changefreq>
 <priority>0.8</priority>
-</url>
-`).join("");
+</url>`;
+}).join("");
 
 const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
 <url>
 <loc>${base}</loc>
 <changefreq>daily</changefreq>
 <priority>1.0</priority>
 </url>
+
 ${urls}
+
 </urlset>`;
 
 res.setHeader("Content-Type", "application/xml");
-res.status(200).send(xml);
+res.statusCode = 200;
+res.write(xml);
+res.end();
 
 } catch (error) {
 
-res.setHeader("Content-Type", "application/xml");
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+</urlset>`;
 
-res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>`);
+res.setHeader("Content-Type", "application/xml");
+res.statusCode = 200;
+res.write(xml);
+res.end();
 
 }
 
