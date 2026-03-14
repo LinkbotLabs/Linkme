22d91a5690df0232fef0defa32e3ce9cdda2906b
David <windowsgames1000@gmail.com>
2026-03-14 20:41:01 +0700

    Update search.js

--- api/search.js
+++ api/search.js
@@ -6,42 +6,7 @@ const cache = {
 
 const ONE_DAY = 1000 * 60 * 60 * 24;
 
-const keywords = [
-  "amazon trending gadgets",
-  "viral kitchen gadgets amazon",
-  "amazon best seller tech",
-  "tiktok viral home gadgets amazon",
-  "amazon impulse buy gadgets",
-  "amazon trending gadgets 2026",
-  "viral amazon kitchen finds 2026",
-  "tiktok viral beauty products amazon",
-  "amazon viral tech gadgets 2026",
-  "trending amazon impulse buys 2026",
-  "amazon best seller kitchen tools 2026",
-  "tiktok viral wellness gadgets amazon",
 
-  "amazon weird gadgets",
-  "amazon problem solving gadgets",
-  "amazon must have gadgets",
-  "tiktok made me buy it amazon",
-  "amazon hidden gem gadgets",
-  "amazon life hack gadgets",
-  "viral amazon home finds",
-  "amazon cool gadgets under 50",
-  "amazon smart home gadgets",
-  "amazon car gadgets trending",
-  "amazon travel gadgets",
-  "amazon desk gadgets",
-  "amazon bedroom gadgets",
-  "amazon bathroom gadgets",
-  "amazon organization gadgets",
-  "amazon cleaning gadgets",
-  "amazon kitchen organization tools"
-  "amazon problem solving kitchen gadgets",
-"amazon space saving gadgets",
-"amazon cleaning hacks gadgets",
-"amazon aesthetic home gadgets"
-];
 export default async function handler(req, res) {
 
   res.setHeader(
