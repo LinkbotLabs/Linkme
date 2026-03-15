03935361534b2c5646450db774410f8d950fa003
David <windowsgames1000@gmail.com>
2026-03-15 10:17:02 +0700

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,5 +1,23 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
+
   const base = "https://floatrising.com";
+
+  let shareIds = [];
+
+  try {
+    const r = await fetch(base + "/api/get");
+    const data = await r.json();
+    shareIds = data.ids || [];
+  } catch (e) {
+    console.log("Share fetch failed");
+  }
+
+  const productUrls = shareIds.map(id => `
+  <url>
+    <loc>${base}/s.html?id=${id}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`).join("");
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -11,16 +29,12 @@ export default function handler(req, res) {
   </url>
 
   <url>
-    <loc>${base}/s.html</loc>
-    <changefreq>daily</changefreq>
-    <priority>0.9</priority>
-  </url>
-
-  <url>
     <loc>${base}/pin.html</loc>
     <changefreq>weekly</changefreq>
     <priority>0.7</priority>
   </url>
+
+${productUrls}
 
 </urlset>`;
 
