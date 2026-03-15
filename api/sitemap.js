71cc4524dae7380791eef31c85a97d7c1b863c06
David <windowsgames1000@gmail.com>
2026-03-15 10:03:42 +0700

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,31 +1,29 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
+  const base = "https://floatrising.com";
 
-const base = "https://floatrising.com";
-
-const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-<url>
-<loc>${base}</loc>
-<changefreq>daily</changefreq>
-<priority>1.0</priority>
-</url>
+  <url>
+    <loc>${base}</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
 
-<url>
-<loc>${base}/s.html</loc>
-<changefreq>daily</changefreq>
-<priority>0.9</priority>
-</url>
+  <url>
+    <loc>${base}/s.html</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
+  </url>
 
-<url>
-<loc>${base}/pin.html</loc>
-<changefreq>weekly</changefreq>
-<priority>0.7</priority>
-</url>
+  <url>
+    <loc>${base}/pin.html</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.7</priority>
+  </url>
 
 </urlset>`;
 
-res.setHeader("Content-Type", "application/xml");
-res.status(200).send(xml);
-
+  res.setHeader("Content-Type", "application/xml");
+  res.status(200).end(xml);
 }
