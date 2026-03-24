84ab8a4282eca6c37371bf0b9d31b0e46c3b1fce
David <windowsgames1000@gmail.com>
2026-03-24 13:23:29 +0700

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -118,7 +118,8 @@ https://floatrising.com`
 
     if (text === "/pack") {
 
-      
+      const apiRes = await fetch("https://floatrising.com/api/feed");
+const data = await apiRes.json();
 
       if (!data.products || data.products.length === 0) {
 
